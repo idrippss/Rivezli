baa0c3d3705b5565fd5e6c6938a79a603c83e6f6
alaeddine <contact@zenhosting.tn>
2024-06-24 18:46:26 +0100

    Add to git

--- resources/views/matieres/edit.blade.php
+++ resources/views/matieres/edit.blade.php
@@ -28,7 +28,13 @@
 
                     <div class="form-group">
                         <label for="langue">Langue</label>
-                        <select class="form-control" id="langue" name="langue" required>
-                            <option value="">Sélectionner une langue</option>
-                            <option value="francais" {{ old('langue', $matiere->langue) == 'francais' ? 'selected' : '' }}>Français</option>
-                            <option value="anglais" {{ old
+                        <input type="text" class="form-control" id="langue" name="langue" value="{{ old('langue', $matiere->langue) }}" required>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary mt-3">Mettre à jour</button>
+                    <a href="{{ route('matieres.index') }}" class="btn btn-secondary mt-3">Annuler</a>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
